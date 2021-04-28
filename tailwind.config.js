991babe42b20bda3b62cb6fa8ba02f2164020960
Ashraf Gehad <deva4rf@gmail.com>
2021-04-28 02:16:38 +0200

    feat: add tailwindcss & customized config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {
+        fontFamily: {
+          sans: ['"Nunito Sans"', ...defaultTheme.fontFamily.sans],
+        },
+        colors: {
+          "moody-blue-50": "#C4C7F3",
+          "moody-blue-100": "#A3A8EB",
+          "moody-blue-200": "#8389E3",
+          "moody-blue-300": "#646BD9",
+          "moody-blue-400": "#464ECF",
+          "moody-blue-500": "#333BB8",
+          "moody-blue-600": "#2C3396",
+          "moody-blue-700": "#242975",
+          "moody-blue-800": "#1C1F55",
+          "moody-blue-900": "#131536"
+        },    
+       },
+     },
+     variants: {
+       extend: {},
+     },
+     plugins: [],
+   }
